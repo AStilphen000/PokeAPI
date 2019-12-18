99de4b634ace8692c66f5115afd56fb095da65b4
astilphen000 <astilphen000@west-mec.org>
2019-12-17 18:19:27 -0700

    .

--- app.js
+++ app.js
@@ -6,108 +6,115 @@ var fetch = require('node-fetch');
 
 // serve static files
 app.use(express.static("public"));
+app.use('/img', express.static('public/img'));
+app.use('/stylesheets', express.static('public/stylesheets'));
+app.use('/css', express.static('public/css'));
+app.use('/lib', express.static('public/lib'));
+app.use('/js', express.static('public/js'));
 
-
-// const resume = {
-//     "basics": {
-//         "name": "Angelina Stilphen",
-//         "label": "Programmer",
-//         "picture": "",
-//         "email": "john@gmail.com",
-//         "phone": "(912) 555-4321",
-//         "website": "http://johndoe.com",
-//         "summary": "A summary of John Doe...",
-//         "location": {
-//             "address": "2712 Broadway St",
-//             "postalCode": "CA 94115",
-//             "city": "San Francisco",
-//             "countryCode": "US",
-//             "region": "California"
-//         },
-//         "profiles": [{
-//             "network": "Twitter",
-//             "username": "john",
-//             "url": "http://twitter.com/john"
-//         }]
-//     },
-//     "work": [{
-//         "company": "Company",
-//         "position": "President",
-//         "website": "http://company.com",
-//         "startDate": "2013-01-01",
-//         "endDate": "2014-01-01",
-//         "summary": "Description...",
-//         "highlights": [
-//             "Started the company"
-//         ]
-//     }],
-//     "volunteer": [{
-//         "organization": "Organization",
-//         "position": "Volunteer",
-//         "website": "http://organization.com/",
-//         "startDate": "2012-01-01",
-//         "endDate": "2013-01-01",
-//         "summary": "Description...",
-//         "highlights": [
-//             "Awarded 'Volunteer of the Month'"
-//         ]
-//     }],
-//     "education": [{
-//         "institution": "University",
-//         "area": "Software Development",
-//         "studyType": "Bachelor",
-//         "startDate": "2011-01-01",
-//         "endDate": "2013-01-01",
-//         "gpa": "4.0",
-//         "courses": [
-//             "DB1101 - Basic SQL"
-//         ]
-//     }],
-//     "awards": [{
-//             "title": "Award",
-//             "date": "2014-11-01",
-//             "awarder": "Company",
-//             "summary": "There is no spoon."
-//         },
-//         {
-//             "title": "Award",
-//             "date": "2014-11-01",
-//             "awarder": "Company",
-//             "summary": "There is no spoon."
-//         }
-//     ],
-//     "publications": [{
-//         "name": "Publication",
-//         "publisher": "Company",
-//         "releaseDate": "2014-10-01",
-//         "website": "http://publication.com",
-//         "summary": "Description..."
-//     }],
-//     "skills": [{
-//         "name": "Web Development",
-//         "level": "Master",
-//         "keywords": [
-//             "HTML",
-//             "CSS",
-//             "Javascript"
-//         ]
-//     }],
-//     "languages": [{
-//         "language": "English",
-//         "fluency": "Native speaker"
-//     }],
-//     "interests": [{
-//         "name": "Wildlife",
-//         "keywords": [
-//             "Ferrets",
-//             "Unicorns"
-//         ]
-//     }],
-//     "references": [{
-//         "name": "Jane Doe",
-//         "reference": "Reference..."
-//     }]
-// };
+const resume = {
+    "basics": {
+        "name": "Angelina Stilphen",
+        "label": "Programmer",
+        "picture": "",
+        "email": "john@gmail.com",
+        "phone": "(912) 555-4321",
+        "website": "http://johndoe.com",
+        "summary": "A summary of John Doe...",
+        "location": {
+            "address": "2712 Broadway St",
+            "postalCode": "CA 94115",
+            "city": "San Francisco",
+            "countryCode": "US",
+            "region": "California"
+        },
+        "profiles": [{
+            "network": "Twitter",
+            "username": "john",
+            "url": "http://twitter.com/john"
+        }]
+    },
+    "work": [{
+        "company": "Company",
+        "position": "President",
+        "website": "http://company.com",
+        "startDate": "2013-01-01",
+        "endDate": "2014-01-01",
+        "summary": "Description...",
+        "highlights": [
+            "Started the company"
+        ]
+    }],
+    "volunteer": [{
+        "organization": "Organization",
+        "position": "Volunteer",
+        "website": "http://organization.com/",
+        "startDate": "2012-01-01",
+        "endDate": "2013-01-01",
+        "summary": "Description...",
+        "highlights": [
+            "Awarded 'Volunteer of the Month'"
+        ]
+    }],
+    "education": [
+        {
+            "institution": "University",
+            "area": "Software Development",
+            "studyType": "Bachelor",
+            "startDate": "2011-01-01",
+            "endDate": "2013-01-01",
+            "gpa": "4.0",
+            "courses": [
+                "DB1101 - Basic SQL"
+            ]
+        }
+    ],
+    "awards": [{
+            "title": "Award",
+            "date": "2014-11-01",
+            "awarder": "Company",
+            "summary": "There is no spoon."
+        },
+        {
+            "title": "Award",
+            "date": "2014-11-01",
+            "awarder": "Company",
+            "summary": "There is no spoon."
+        }
+    ],
+    "publications": [{
+        "name": "Publication",
+        "publisher": "Company",
+        "releaseDate": "2014-10-01",
+        "website": "http://publication.com",
+        "summary": "Description..."
+    }],
+    "skills": [
+        {
+            "name": "HTML",
+            "level": "90"
+        },
+        {
+            "name": "CSS",
+            "level": "90"
+        }
+    ],
+    "languages": [{
+        "language": "English",
+        "fluency": "Native speaker"
+    }],
+    "interests": [{
+        "name": "Wildlife",
+        "keywords": [
+            "Ferrets",
+            "Unicorns"
+        ]
+    }],
+    "references": [{
+        "name": "Jane Doe",
+        "reference": "Reference..."
+    }]
+};
 
 
 
@@ -123,6 +130,7 @@ app.use(express.static("public"));
     - [app.set](https://expressjs.com/en/api.html#app.set)
 */
 const hbs = require('express-handlebars')({
+    defaultLayout: "main",
     extname: '.hbs',
   });
   app.engine('hbs', hbs);
@@ -134,7 +142,8 @@ const hbs = require('express-handlebars')({
 */
 app.get("/", (request, res) => {
     res.render('index', {
-      name: resume.basics.name
+      name: resume.basics.name,
+      skills: resume.skills
     });
   });
 
