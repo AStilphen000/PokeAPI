ddb69018713383a8aa31d2a01f73773caf1e7747
astilphen000 <astilphen000@west-mec.org>
2020-01-15 18:19:02 -0700

    Created first PHP HTML file

---
+++ views/layouts/PHPCodeBlocks.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Code Blocks</title>
+<!-- 
+Exercise 02_02_01
+Author: Angelina Stiphen
+Date: 1.15.20
+
+-->
+</head>
+<body>
+    <p>
+    <?php 
+    echo "This test is displayed using standard PHP script delimiters<br>";
+    print "Second string";
+    ?>
+    </p>
+</body>
+</html>
---
+++ views/layouts/PHPtest.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Code Blocks</title>
+<!-- 
+Exercise 02_02_01
+Author: Angelina Stilphen
+Date: 1.15.20
+
+-->
+</head>
+<body>
+    <p>
+    <?php 
+    phpinfo();  
+    ?>
+    </p>
+</body>
+</html>
