e59f3c61cbb32031df6eceff53d1e9dc11c07350
astilphen000 <astilphen000@west-mec.org>
2020-01-22 18:24:04 -0700

    Working with constants
    Using constants with variables

---
+++ views/layouts/HelloWorld2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello World</title>
+    <!-- 
+        Author: Angelina Stilphen
+        Date: 1.22.20
+        Exercise hello world 2
+     -->
+</head>
+<body>
+<?php
+    $worldVar = "World";
+    $sunVar = "Sun";
+    $moonVar = "Moon";
+    define("WORLD_INFO", 92897000);
+    define("SUN_INFO", 72000000);
+    define("MOON_INFO", 3456);
+    ?>
+    
+    <?php
+    echo "<p>Hello $worldVar!<br>";
+    echo "The $worldVar is ",WORLD_INFO," miles from the $sunVar.<br>";
+    echo "Hello", $sunVar, "!<br>";
+    echo "Hello", $moonVar, "!<br>";
+    echo "The $moonVar is ",MOON_INFO," miles in diameter. </p>";
+    ?>
+</body>
+</html>
