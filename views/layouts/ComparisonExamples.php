7a6843e3273b3f654b4fecbca4652b34c358153c
astilphen000 <astilphen000@west-mec.org>
2020-01-29 18:22:36 -0700

    Relational Equal Operators
    Adde a few different relational equal operators and not equal operators.

---
+++ views/layouts/ComparisonExamples.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comparison Examples</title>
+    <!-- 
+        Author: Angelina Stilphen
+        Date: 1.29.20
+        Exercise 02_01_01
+     -->
+</head>
+<body>
+    <?php
+    echo "<p>";
+    echo "<h2>Relational Equal Operator</h2>";
+    $value1 = "first text string";
+    $value2 = "second text string";
+    $returnValue = ($value1 == $value2 ? "true" : "false");
+    echo 'value1: ', $value1, ' $value2: ', $value2, '<br>';
+    echo 'After $value1 == $value2 $returnValue is: ',
+        $returnValue, "<br>";
+    echo "<h2>Relational Equal Operator</h2>";
+    $value1 = 50;
+    $value2 = 75;
+    $returnValue = ($value1 == $value2 ? "true" : "false");
+    echo '$value1: ', $value1, ' $value2: ', $value2, '<br>';
+    echo 'After $value1 == $value2 $returnValue is: ',
+        $returnValue, "<br>";
+    echo "<h2>Relational Not Equal Operator</h2>";
+    $returnValue = ($value1 != $value2 ? "true" : "false");
+    echo '$value1: ', $value1, ' $value2: ', $value2, '<br>';
+    echo 'After $value1 != $value2 $returnValue is: ',
+        $returnValue, "<br>";
+    echo "<h2>Relational Alternative Not Equal Operators</h2>";
+    $returnValue = ($value1 <> $value2 ? "true" : "false");
+    echo 'value1: ', $value1, ' $value2: ', $value2, "<br>";
+    echo 'After $value1 <> $value2 $returnValue is: ', 
+        $returnValue, "<br>";
+    ?>
+</body>
+</html>
